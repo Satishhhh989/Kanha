eb3a95078047a201bea916aa808f48765584fd99
Satish Chawan <satishchawan1818@gmail.com>
2026-09-22 10:09:17 +0530

    fix(cli): eliminate runpy module reload warning

--- apps/cli/__init__.py
+++ apps/cli/__init__.py
@@ -1,3 +1,7 @@
-from .main import app
+def __getattr__(name: str):
+    if name == "app":
+        from .main import app
+        return app
+    raise AttributeError(f"module '{__name__}' has no attribute '{name}'")
 
 __all__ = ["app"]
